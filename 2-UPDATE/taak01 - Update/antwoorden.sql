59436c21399c1717deb477accad469268e47685b
Jayson1307 <2125083@talnet.nl>
2021-02-15 12:46:16 +0100

    update taak 1

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,24 @@
 -- Opdracht 1
+UPDATE studenten2
+set woonplaats = 'Amstelveen'
 
 -- Opdracht 2 
-
+UPDATE studenten2
+set adres = 'ergens', postcode = '1234AB'
+wehere student_id = ...
 -- Opdracht 3
-
+UPDATE studenten2
+set geboortedatum = '2000-12-12'
+wehere student_id = 2
 -- Opdracht 4
+UPDATE studenten2
+set klas = '9C'
+where student_id = 4
 
+UPDATE studenten2
+set klas = '9A'
+where student_id = 8
 -- Opdracht 5
+UPDATE studenten2
+set voornaam = 'Johannah'
+WHERE student_id = 6
