c67aa49334971d53129250420e7ead4208e0e5ce
Jayson1307 <2125083@talnet.nl>
2021-02-15 13:17:55 +0100

    update opdracht 2

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,16 @@
 -- Opdracht 1
-
+UPDATE studenten2
+set klas = '9X'
+where klas = '9A'
 -- Opdracht 2 
-
+UPDATE studenten2
+set klas = '9Z'
+where student_id > 7 
 -- Opdracht 3
-
+UPDATE studenten2
+set woonplaats = 'Amsterdam'
+where student_id <=2 OR student_id > 7
 -- Opdracht 4
-
+UPDATE studenten2
+set woonplaats = 'Uithoorn'
+where klas = '9X' AND woonplaats = 'Amsterdam'
